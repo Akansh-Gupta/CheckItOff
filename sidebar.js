18e10fce8500684714499f477210a821ad18a341
Akansh-Gupta <akg.gupta365@gmail.com>
2025-01-16 22:41:29 +0530

    Minor changes

--- sidebar.js
+++ sidebar.js
@@ -5,7 +5,7 @@ sidebar.innerHTML = `
             <span><img src="images/CheckItOff.png" alt="" class="logo-img" id="btn"></span>
         </div>
         <ul class="sidebarUl">
-            <li><a href="todo.html"><i class='bx bx-check-square'></i><span class="nav-item">ToDo</span></a></li>
+            <li><a href="index.html"><i class='bx bx-check-square'></i><span class="nav-item">ToDo</span></a></li>
             <li><a href="notes.html"><i class='bx bx-edit'></i><span class="nav-item">Notes</span></a></li>
             <li><a href="reminder.html"><i class="bx bx-list-check"></i><span class="nav-item">Reminder</span></a></li>
         </ul>
